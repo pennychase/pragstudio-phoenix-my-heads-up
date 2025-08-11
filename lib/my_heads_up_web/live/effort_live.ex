8c2691b61686d89abf0e0723b2bb92a6a8cc72ff
Penny Chase <mpchase@mac.com>
2025-08-11 18:47:07 -0400

    Video 10 exercise - inspect processes to see websocket process

--- lib/my_heads_up_web/live/effort_live.ex
+++ lib/my_heads_up_web/live/effort_live.ex
@@ -2,6 +2,7 @@ defmodule MyHeadsUpWeb.EffortLive do
   use MyHeadsUpWeb, :live_view
 
   def mount(_params, _session, socket) do
+    # IO.inspect(self(), label: "MOUNT")
 
     socket = assign(socket, responders: 0, minutes_per_responder: 10)
 
@@ -9,24 +10,37 @@ defmodule MyHeadsUpWeb.EffortLive do
   end
 
   def render(assigns) do
+    # IO.inspect(self(), label: "RENDER")
+
     ~H"""
     <div class="effort">
     <h1>Community Love</h1>
       <section>
+        <button phx-click="add" phx-value-quantity="3">
+          + 3
+        </button>
         <div>
-          <%= @responders %>
+          {@responders}
         </div>
         &times;
         <div>
-          <%= @minutes_per_responder %>
+          {@minutes_per_responder}
         </div>
         =
         <div>
-          <%= @responders * @minutes_per_responder %>
+          {@responders * @minutes_per_responder}
         </div>
       </section>
     </div>
     """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    # IO.inspect(self(), label: "ADD")
+
+    socket = update(socket, :responders, &(&1 + String.to_integer(quantity)))
+
+    {:noreply, socket}
   end
   
 end
