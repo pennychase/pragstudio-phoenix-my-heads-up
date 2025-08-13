da9d846429d91c584852c3b30631804823435e2c
Penny Chase <mpchase@mac.com>
2025-08-13 19:49:22 -0400

    Video 14 - Rewrote /incident rendering with function components

--- lib/my_heads_up_web/live/incident_live/index.ex
+++ lib/my_heads_up_web/live/incident_live/index.ex
@@ -13,19 +13,37 @@ defmodule MyHeadsUpWeb.IncidentLive.Index do
     ~H"""
     <div class="incident-index">
       <div class="incidents">
-        <div :for={incident <- @incidents} class="card">
-          <img src={incident.image_path} />
-          <h2>[incident.name}</h2>
-          <div class="details">
-            <div class="badge">
-              {incident.status}
-            </div>
-            <div class="priority">
-              {incident.priority}
-            </div>
-          </div>
+        <.incident_card :for={incident <- @incidents} incident={incident} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :incident, MyHeadsUp.Incident, required: true
+
+  def incident_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@incident.image_path} />
+      <h2>{@incident.name}</h2>
+      <div class="details">
+        <div class="badge">
+          <.badge status={@incident.status} />
+        </div>
+        <div class="priority">
+          {@incident.priority}
         </div>
       </div>
+    </div>
+    """   
+  end
+
+  attr :badge, :atom, values: [:pending, :resolved, :canceled], default: :pending
+
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      {@status}
     </div>
     """
   end
