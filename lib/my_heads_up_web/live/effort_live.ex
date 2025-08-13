a9196147f594156d5903bd598c795e1864b0a1e6
Penny Chase <mpchase@mac.com>
2025-08-12 20:10:03 -0400

    Video 12 - automatically update number of responders

--- lib/my_heads_up_web/live/effort_live.ex
+++ lib/my_heads_up_web/live/effort_live.ex
@@ -4,6 +4,10 @@ defmodule MyHeadsUpWeb.EffortLive do
   def mount(_params, _session, socket) do
     # IO.inspect(self(), label: "MOUNT")
 
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     socket = assign(socket, responders: 0, minutes_per_responder: 10)
 
     {:ok, socket}
@@ -50,6 +54,13 @@ defmodule MyHeadsUpWeb.EffortLive do
 
   def handle_event("recalculate", %{"minutes_per_responder" => minutes_per_responder}, socket) do
     socket = assign(socket, :minutes_per_responder, String.to_integer(minutes_per_responder))
+
+    {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    socket = update(socket, :responders, &(&1 + 3))
 
     {:noreply, socket}
   end
