b74af28c6eaff8c6d4e68981ab31c856f7631b72
Penny Chase <mpchase@mac.com>
2025-09-01 16:29:00 -0400

    Video 39 Exercise - Javascript commands

--- lib/my_heads_up_web/live/admin_incident_live/index.ex
+++ lib/my_heads_up_web/live/admin_incident_live/index.ex
@@ -17,15 +17,29 @@ defmodule MyHeadsUpWeb.AdminIncidentLive.Index do
   def render(assigns) do
     ~H"""
     <div class="admin-index">
+      <.button phx-click ={
+        JS.toggle(
+          to: "#joke",
+          in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+          out: {"ease-in-out duration-300", "opacity-100", "opacity-0"},
+          time: 300
+        )
+      }>
+        Toggle Joke
+      </.button>
+      <div id="joke" class="joke hidden" phx-click={JS.toggle_class("animate-bounce")}>
+        Why shouldn't you trust trees?
+      </div>
       <.header>
-        <%= @page_title %>
         <:actions>
           <.link navigate={~p"/admin/incidents/new"} class="button">
             New Incident
           </.link>
         </:actions>
       </.header>
-      <.table id="incidents" rows={@streams.incidents}>
+      <.table id="incidents" 
+              rows={@streams.incidents}
+              row_click={fn {_, incident} -> JS.navigate(~p"/incidents/#{incident}") end}>
         <:col :let={{_dom_id, incident}} label="Name">
           <.link navigate={~p"/incidents/#{incident}"}>
             <%= incident.name %>
@@ -42,8 +56,10 @@ defmodule MyHeadsUpWeb.AdminIncidentLive.Index do
           Edit
         </.link>
       </:action>
-      <:action :let={{_dom_id, incident}}>
-        <.link phx-click="delete" phx-value-id={incident.id} data-confirm="Are you sure?">
+      <:action :let={{dom_id, incident}}>
+        <.link phx-click={JS.push("delete", value: %{id: incident.id})
+                          |> JS.hide(to: "##{dom_id}", transition: "fade-out")
+                        } phx-value-id={incident.id} data-confirm="Are you sure?">
           <.icon name="hero-trash" class="h-4 w-4" />
         </.link>
       </:action>
