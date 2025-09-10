ad5c2aa6a7feb614b2258516831930ecd15aaf30
Penny Chase <mpchase@mac.com>
2025-09-10 17:44:04 -0400

    Video 47 - Generated Auth System

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :my_heads_up, :scopes,
+  user: [
+    default: true,
+    module: MyHeadsUp.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: MyHeadsUp.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :my_heads_up,
   ecto_repos: [MyHeadsUp.Repo],
   generators: [timestamp_type: :utc_datetime]
--- lib/my_heads_up_web/router.ex
+++ lib/my_heads_up_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MyHeadsUpWeb.Router do
   use MyHeadsUpWeb, :router
 
+  import MyHeadsUpWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule MyHeadsUpWeb.Router do
     plug :put_root_layout, html: {MyHeadsUpWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
     plug :snoop
   end
 
@@ -70,5 +73,33 @@ defmodule MyHeadsUpWeb.Router do
       live_dashboard "/dashboard", metrics: MyHeadsUpWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", MyHeadsUpWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MyHeadsUpWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", MyHeadsUpWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{MyHeadsUpWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
